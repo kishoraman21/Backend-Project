434ad2c403293d35143a42915fc8f5ae180f1c57
kishoraman21 <kishoraman2121@gmail.com>
2025-11-23 01:25:08 +0530

    fixed bug (await)

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -55,9 +55,11 @@ const UserSchema = new Schema(
   }
 );
 
+//hashing password
+
 UserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
