7b32927678c415c120b9248a8c86fb0b7ef8ff1c
kishoraman21 <kishoraman2121@gmail.com>
2025-11-23 20:40:37 +0530

    fixed the return keyword

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -7,18 +7,14 @@
 //             message:err.message
 //         })
 //     }
-// 
-
-
-
+//
 
 // 2nd method
 
-const asyncHandler = (requestHandler) =>{
-    (req,res,next)=>{
-        Promise.resolve(requestHandler(req,res,next)).
-        catch((err)=> next(err))
-    }
-}
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
-export {asyncHandler}
+export { asyncHandler };
